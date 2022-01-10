d4ae916e3ae63ae3358fdafd460da0040879a336
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2022-01-10 08:18:51 +0100

    Initial work on "refresh-content" node script
    This will later be used to build and invalidate cache for the site content

--- scripts/refresh-content.js
+++ scripts/refresh-content.js
@@ -1,4 +1,25 @@
-const [changedFiles] = process.argv.slice(2)
+const [changedFiles] = process.argv.slice(2);
 
-const changedFilesPaths = changedFiles.split(',')
-console.log(changedFilesPaths)
+const run = async () => {
+  //Get changed files paths from args
+  const changedFilesPaths = changedFiles?.split(",") || [];
+
+  // Filter changed files not from content, even if there shouldn't be
+  const changedContentFiles = changedFilesPaths.filter((file) =>
+    file.startsWith("content")
+  );
+
+  if (changedContentFiles.length) {
+    const changedFilesName = changedContentFiles.map((file) =>
+      file.replace(/^content\//, "")
+    );
+    console.log(changedFilesName);
+  } else {
+    // Files are checked on GH action, so there should always be changed files if this script is executed
+    console.log(
+      'No changed files found, this should not happen, check the "refresh-content" GH action!'
+    );
+  }
+};
+
+run();
